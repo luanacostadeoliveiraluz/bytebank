95b20a50c156b809af4931e5ffb40db39d5d17a2
user <https://github.com/luanacostadeoliveiraluz/bytebankprojeto.git>
2021-05-21 01:58:59 -0300

    bytebank

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,50 @@
-
-import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-void main() => runApp(BytebankApp());
-
-class BytebankApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: ListaTransferencias(),
-        appBar: AppBar(
-          title: Text('Transferências'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-        body: FormularioTransferencia(),
-      ),
-    ));
-    );
-  }
-}
-
-class FormularioTransferencia extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Criando Transferência'),),
-        body: Text('teste'));
-  }
-}
+ void main() async {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Column(
+          children: <Widget>[
+      Card(
+      child: ListTile(
+          leading: Icon(Icons.monetization_on),
+      title: Text('100.0'),
+      subtitle: Text('1000'),
+    )
+    home: Scaffold(
+    body: ListaTransferencias(),
+  appBar: AppBar(
+    title: Text('Transferências'),
+  ),
+  Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text('200.0'),
+        subtitle: Text('1000'),
+      )
+      floatingActionButton: FloatingActionButton(
+  child: Icon(Icons.add),
+),
+),
+));
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: <Widget>[
-          ItemTransferencia(Transferencia(100.0, 1000)),
-          ItemTransferencia(Transferencia(200.0, 2000)),
-          ItemTransferencia(Transferencia(300.0, 3000)),
-        ],
-        return Scaffold(
-    appBar: AppBar(
-    title: Text('Transferências'),
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0, 1000)),
+        ItemTransferencia(Transferencia(200.0, 2000)),
+        ItemTransferencia(Transferencia(300.0, 3000)),
+      ],
     ),
-    body: Column(
-    children: <Widget>[
-    ItemTransferencia(Transferencia(100.0, 1000)),
-    ItemTransferencia(Transferencia(200.0, 2000)),
-    ItemTransferencia(Transferencia(300.0, 3000)),
-    ],
-    ),
+    appBar: AppBar(title: Text('Transferências'),),
     floatingActionButton: FloatingActionButton(
     child: Icon(Icons.add),
     ),
+    ),
+    ));
     );
+}
   }
 }
